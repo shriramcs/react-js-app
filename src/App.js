81230e8ac7a6454ce31ab77303655a396c80eccf
Shriram Sapparad <shriramcs@gmail.com>
2021-06-12 15:40:59 +0530

    project set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import LandingPage from "./LandingPage";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello React JS</h1>
+      <h1 className="Header">React JS</h1>
+      <LandingPage />
     </div>
   );
 }
---
+++ src/LandingPage.jsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <h2>Welcome 💚</h2>
+    </div>
+  );
+}
